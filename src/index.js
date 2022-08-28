2f0eb4200c1759321a5dc40d564288f3a98cccde
rmohanmd <rmohan.md@gmail.com>
2022-08-28 04:43:09 -0400

    Added message prop to pass on message content from parent to child

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import { faker } from "@faker-js/faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 2:00PM"
+        message="Test 123"
+        avatarPoster="faker.image.avatar()"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 11:25AM"
+        message="Love the design"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 7:00PM"
+        message="This is great!"
+      />
     </div>
   );
 };
