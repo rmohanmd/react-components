18dd8590690f23fe3aebac4f8ab1a300e87f081d
rmohanmd <rmohan.md@gmail.com>
2022-08-28 14:24:50 -0400

    Moved all CommentDetail elements under the ApprovalCard element as child props

--- src/index.js
+++ src/index.js
@@ -15,18 +15,22 @@ const App = () => {
           avatarPoster={faker.image.avatar()}
         />
       </ApprovalCard>
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 11:25AM"
-        message="Love the design"
-        avatarPoster={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 7:00PM"
-        message="This is great!"
-        avatarPoster={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 11:25AM"
+          message="Love the design"
+          avatarPoster={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 7:00PM"
+          message="This is great!"
+          avatarPoster={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
